315be619bafd6a1298e66bd9dc593af8523207d8
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-11-03 23:03:38 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div>Welcome Here</div>
+  </StrictMode>,
+)
