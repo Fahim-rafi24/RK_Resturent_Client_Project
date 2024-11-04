a50db0e6282536bb001cab96dc7ce649be9e3be3
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-11-04 16:14:05 +0600

    install react icon

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom"
-import ErrorBg from "../../assets/error.svg" 
+import ErrorBg from "../../assets/error.svg"
+import { CiLocationArrow1 } from "react-icons/ci";
 
-export default function ErrorPage(){
-    return(
+
+
+
+
+export default function ErrorPage() {
+    return (
         <Link
-        to={'/'}
-        className="h-[95vh] flex items-center justify-center">
-        <img src={ErrorBg} alt="Error Page" className="w-[40vw] h-[60vh]"/>
-        {/* <button></button> */}
+            to={'/'}
+            className="h-[95vh] flex flex-col items-center justify-center">
+            <button className="btn btn-outline">
+                Go Home
+                <CiLocationArrow1 className="text-2xl" />
+            </button>
+            <img src={ErrorBg} alt="Error Page" className="w-[40vw] h-[60vh]" />
         </Link>
     )
 }
