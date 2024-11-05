fcac1b01f941d8c2c044c47e5e6eaf454852ac76
Fahim Rafi <fahimrafibd2003@gmail.com>
2024-11-05 15:10:42 +0600

    install @smastrom/react-rating

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ Here use helping Components :-
 06. react-responsive-carousel
 07. swiper-js
 08. axios
+09. @smastrom/react-rating
--- src/Components/Comment/Comment.jsx
+++ src/Components/Comment/Comment.jsx
@@ -1,3 +1,5 @@
+import queationImg from "../../assets/HomeBanner/quotation.jpg"
+
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -8,6 +10,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { useEffect, useState } from "react"
 import { axiosInstalled } from "../../Axios/Axios"
 import RoyelDivBar from "../../CommonThink/RoyelTitleBar/RoyelDivBar";
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
 
 
 
@@ -31,6 +35,10 @@ export default function Comment() {
                 {
                     commenst.map(each => {
                         return <SwiperSlide key={each._id}>
+                            <div className='flex flex-col items-center'>
+                            <Rating style={{ maxWidth: 200 }} value={each.rating} readOnly/>
+                            <img src={queationImg} className="w-20 my-10"/>
+                            </div>
                             <div className='flex flex-col h-80 md:h-48'>
                                 <p className='flex-grow raleway'>{each.details}</p>
                                 <h2 className='text-2xl font-bold text-[#CD9003] mt-5 cinzel'>{each.name}</h2>
